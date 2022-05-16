52f8e40752a356cbe2e8e7e8ff97a95490de18c5
laurenkumar <lauren_d@outlook.fr>
2022-05-17 01:23:57 +0200

    first commit

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -133,7 +133,7 @@ const HomePageContent: React.FC = () => {
            <div className="video-time">8 min</div>
            <div className="video-wrapper">
             <video muted="">
-             <source src="https://player.vimeo.com/external/436572488.sd.mp4?s=eae5fb490e214deb9ff532dd98d101efe94e7a8b&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
+             <source src="https://player.vimeo.com/external/436572488.sd.mp4?s=eae5fb490e214deb9ff532dd98d101efe94e7a8b&profile_id=139&oauth2_token_id=57447761" type="video/mp4" />
             </video>
             <div className="author-img__wrapper video-author">
              <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="feather feather-check">
@@ -150,7 +150,7 @@ const HomePageContent: React.FC = () => {
            <div className="video-time">5 min</div>
            <div className="video-wrapper">
             <video muted="">
-             <source src="https://player.vimeo.com/external/449972745.sd.mp4?s=9943177fe8a6147b7bc4598259401f06ec57878a&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
+             <source src="https://player.vimeo.com/external/449972745.sd.mp4?s=9943177fe8a6147b7bc4598259401f06ec57878a&profile_id=139&oauth2_token_id=57447761" type="video/mp4" />
             </video>
             <div className="author-img__wrapper video-author">
              <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="feather feather-check">
@@ -167,7 +167,7 @@ const HomePageContent: React.FC = () => {
            <div className="video-time">7 min</div>
            <div className="video-wrapper">
             <video muted="">
-             <source src="https://player.vimeo.com/external/436553499.sd.mp4?s=0e44527f269278743db448761e35c5e39cfaa52c&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
+             <source src="https://player.vimeo.com/external/436553499.sd.mp4?s=0e44527f269278743db448761e35c5e39cfaa52c&profile_id=139&oauth2_token_id=57447761" type="video/mp4" />
             </video>
             <div className="author-img__wrapper video-author">
              <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="feather feather-check">
@@ -184,7 +184,7 @@ const HomePageContent: React.FC = () => {
            <div className="video-time">6 min</div>
            <div className="video-wrapper">
             <video muted="">
-             <source src="https://player.vimeo.com/external/361861493.sd.mp4?s=19d8275ca755d653042a87ef28b2f0b2eabf57d0&profile_id=139&oauth2_token_id=57447761" type="video/mp4">
+             <source src="https://player.vimeo.com/external/361861493.sd.mp4?s=19d8275ca755d653042a87ef28b2f0b2eabf57d0&profile_id=139&oauth2_token_id=57447761" type="video/mp4" />
             </video>
             <div className="author-img__wrapper video-author">
              <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="feather feather-check">
