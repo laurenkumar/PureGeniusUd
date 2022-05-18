037a946acfc6936d21f6e3514b2d82616167b2f2
laurenkumar <lauren_d@outlook.fr>
2022-05-18 14:09:16 +0200

    first commit

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -130,24 +130,28 @@ const HomePageContent: React.FC = () => {
             </div>
           </div>
           <div className='main-container'>
-            <div className='main-header anim' style='--delay: 0s'>Discover</div>
-              <div className='main-blogs'>
-                <div className='main-blog anim' style='--delay: .1s'>
-                  <div className='main-blog__title'>How to do Basic Jumping and how to landing safely</div>
-                  <div className='main-blog__author'>
-                    <div className='author-img__wrapper'>
-                      <svg viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='3' strokeLinecap='round' strokeLinejoin='round' className='feather feather-check'>
-                        <path d='M20 6L9 17l-5-5' />
-                      </svg>
-                      <img className='author-img' src='https://images.unsplash.com/photo-1560941001-d4b52ad00ecc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80' />
-                    </div>
-                    <div className='author-detail'>
-                      <div className='author-name'>Thomas Hope</div>
-                      <div className='author-info'>53K views <span className='seperate'></span>2 weeks ago</div>
-                    </div>
-                  </div>
-                  <div className='main-blog__time'>7 min</div>
+            <div className='main-header anim' style='--delay: 0s'>
+              Discover
+            </div>
+            <div className='main-blogs'>
+              <div className='main-blog anim' style='--delay: .1s'>
+                <div className='main-blog__title'>
+                  How to do Basic Jumping and how to landing safely
                 </div>
+                <div className='main-blog__author'>
+                  <div className='author-img__wrapper'>
+                    <svg viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='3' strokeLinecap='round' strokeLinejoin='round' className='feather feather-check'>
+                      <path d='M20 6L9 17l-5-5' />
+                    </svg>
+                    <img className='author-img' src='https://images.unsplash.com/photo-1560941001-d4b52ad00ecc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80' />
+                  </div>
+                  <div className='author-detail'>
+                    <div className='author-name'>Thomas Hope</div>
+                    <div className='author-info'>53K views <span className='seperate'></span>2 weeks ago</div>
+                  </div>
+                </div>
+                <div className='main-blog__time'>7 min</div>
+              </div>
               <div className='main-blog anim' style='--delay: .2s'>
                 <div className='main-blog__title'>Skateboard Tips You need to know</div>
                 <div className='main-blog__author tips'>
