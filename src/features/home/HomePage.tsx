db0690676be17060714127bbe5b8558cc7afef39
laurenkumar <lauren_d@outlook.fr>
2022-05-18 14:55:17 +0200

    first commit

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -233,7 +233,8 @@ const HomePageContent: React.FC = () => {
                   Basic how to ride your skateboard comfortly
                 </div>
                 <div className='video-view'>
-                  54K views<span className='seperate video-seperate'></span>1 week ago
+                  54K views<span className='seperate video-seperate'></span>
+                  1 week ago
                 </div>
               </div>
               <div className='video anim' style='--delay: .5s'>
@@ -268,7 +269,8 @@ const HomePageContent: React.FC = () => {
                   Prepare for your first skateboard jump
                 </div>
                 <div className='video-view'>
-                  42K views<span className='seperate video-seperate'></span>1 week ago
+                  42K views<span className='seperate video-seperate'></span>
+                  1 week ago
                 </div>
               </div>
               <div className='video anim' style='--delay: .6s'>
@@ -303,7 +305,8 @@ const HomePageContent: React.FC = () => {
                   Basic equipment to play skateboard safely
                 </div>
                 <div className='video-view'>
-                  64K views<span className='seperate video-seperate'></span>2 week ago
+                  64K views<span className='seperate video-seperate'></span>
+                  2 week ago
                 </div>
               </div>
               <div className='video anim' style='--delay: .7s'>
@@ -338,13 +341,14 @@ const HomePageContent: React.FC = () => {
                   Tips to playing skateboard on the ramp
                 </div>
                 <div className='video-view'>
-                  50K views<span className='seperate video-seperate'></span>1 week ago
+                  50K views<span className='seperate video-seperate'></span>
+                  1 week ago
                 </div>
               </div>
             </div>
             <div className='stream-area'>
               <div className='video-stream'>
-                <video 
+                <video
                   id='my_video_1'
                   className='video-js vjs-default-skin anim'
                   width='640px'
@@ -390,7 +394,7 @@ const HomePageContent: React.FC = () => {
                         <button className='like'>
                           <svg
                             viewBox='0 0 24 24'
-                            fill='currentColor' 
+                            fill='currentColor'
                             xmlns='http://www.w3.org/2000/svg'
                           >
                             <path d='M21.435 2.582a1.933 1.933 0 00-1.93-.503L3.408 6.759a1.92 1.92 0 00-1.384 1.522c-.142.75.355 1.704 1.003 2.102l5.033 3.094a1.304 1.304 0 001.61-.194l5.763-5.799a.734.734 0 011.06 0c.29.292.29.765 0 1.067l-5.773 5.8c-.428.43-.508 1.1-.193 1.62l3.075 5.083c.36.604.98.946 1.66.946.08 0 .17 0 .251-.01.78-.1 1.4-.634 1.63-1.39l4.773-16.075c.21-.685.02-1.43-.48-1.943z' />
@@ -412,9 +416,21 @@ const HomePageContent: React.FC = () => {
                         </button>
                       </div>
                     </div>
-                    <div className='video-p-title anim' style='--delay: .2s'>Basic how to ride your Skateboard</div>
-                    <div className='video-p-subtitle anim' style='--delay: .3s'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus illum tempora consequuntur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis earum velit accusantium maiores qui sit quas, laborum voluptatibus vero quidem tempore facilis voluptate tempora deserunt! </div>
-                    <div className='video-p-subtitle anim' style='--delay: .4s'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus laborum qui dolorum fugiat eius accusantium repellendus illum tempora consequuntur. Lorem ipsum dolor, sit amet consectetur adipisicing elit.</div>
+                    <div className='video-p-title anim' style='--delay: .2s'>
+                      Basic how to ride your Skateboard
+                    </div>
+                    <div className='video-p-subtitle anim' style='--delay: .3s'>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Repellendus illum tempora consequuntur. Lorem ipsum dolor
+                      sit amet consectetur adipisicing elit. Debitis earum velit accusantium maiores qui sit quas, laborum voluptatibus 
+                      vero quidem tempore facilis voluptate tempora deserunt!
+                    </div>
+                    <div className='video-p-subtitle anim' style='--delay: .4s'>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Temporibus laborum qui dolorum fugiat eius accusantium 
+                      repellendus illum tempora consequuntur. Lorem ipsum dolor, 
+                      sit amet consectetur adipisicing elit.
+                    </div>
                   </div>
                 </div>
               </div>
@@ -458,7 +474,9 @@ const HomePageContent: React.FC = () => {
                       </div>
                       <div className='msg-wrapper'>
                         <div className='msg__name video-p-name'> Wijaya Adabi</div>
-                        <div className='msg__content video-p-sub'> Lorem ipsum clor sit, ame conse quae debitis</div>
+                        <div className='msg__content video-p-sub'>
+                          Lorem ipsum clor sit, ame conse quae debitis
+                        </div>
                       </div>
                     </div>
                     <div className='message anim' style='--delay: .2s'>
