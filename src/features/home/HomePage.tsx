dcbeeef8c598a05c732cc886e4494a845231b69e
laurenkumar <lauren_d@outlook.fr>
2022-05-18 14:49:02 +0200

    first commit

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -158,13 +158,17 @@ const HomePageContent: React.FC = () => {
                   </div>
                   <div className='author-detail'>
                     <div className='author-name'>Thomas Hope</div>
-                    <div className='author-info'>53K views <span className='seperate'></span>2 weeks ago</div>
+                    <div className='author-info'>
+                      53K views <span className='seperate'></span>2 weeks ago
+                    </div>
                   </div>
                 </div>
                 <div className='main-blog__time'>7 min</div>
               </div>
               <div className='main-blog anim' style='--delay: .2s'>
-                <div className='main-blog__title'>Skateboard Tips You need to know</div>
+                <div className='main-blog__title'>
+                  Skateboard Tips You need to know
+                </div>
                 <div className='main-blog__author tips'>
                   <div className='main-blog__time'>7 min</div>
                   <div className='author-img__wrapper'>
@@ -186,18 +190,25 @@ const HomePageContent: React.FC = () => {
                   </div>
                   <div className='author-detail'>
                     <div className='author-name'>Tony Andrew</div>
-                    <div className='author-info'>53K views <span></span>2 weeks ago</div>
+                    <div className='author-info'>
+                      53K views <span></span>2 weeks ago
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div className='small-header anim' style='--delay: .3s'>Most Watched</div>
+            <div className='small-header anim' style='--delay: .3s'>
+              Most Watched
+            </div>
             <div className='videos'>
               <div className='video anim' style='--delay: .4s'>
                 <div className='video-time'>8 min</div>
                 <div className='video-wrapper'>
                   <video muted=''>
-                    <source src='https://player.vimeo.com/external/436572488.sd.mp4?s=eae5fb490e214deb9ff532dd98d101efe94e7a8b&profile_id=139&oauth2_token_id=57447761' type='video/mp4' />
+                    <source
+                      src='https://player.vimeo.com/external/436572488.sd.mp4?s=eae5fb490e214deb9ff532dd98d101efe94e7a8b&profile_id=139&oauth2_token_id=57447761'
+                      type='video/mp4'
+                    />
                   </video>
                   <div className='author-img__wrapper video-author'>
                     <svg
@@ -218,14 +229,21 @@ const HomePageContent: React.FC = () => {
                   </div>
                 </div>
                 <div className='video-by'>Andy William</div>
-                <div className='video-name'>Basic how to ride your skateboard comfortly</div>
-                <div className='video-view'>54K views<span className='seperate video-seperate'></span>1 week ago</div>
+                <div className='video-name'>
+                  Basic how to ride your skateboard comfortly
+                </div>
+                <div className='video-view'>
+                  54K views<span className='seperate video-seperate'></span>1 week ago
+                </div>
               </div>
               <div className='video anim' style='--delay: .5s'>
                 <div className='video-time'>5 min</div>
                 <div className='video-wrapper'>
                   <video muted=''>
-                    <source src='https://player.vimeo.com/external/449972745.sd.mp4?s=9943177fe8a6147b7bc4598259401f06ec57878a&profile_id=139&oauth2_token_id=57447761' type='video/mp4' />
+                    <source
+                      src='https://player.vimeo.com/external/449972745.sd.mp4?s=9943177fe8a6147b7bc4598259401f06ec57878a&profile_id=139&oauth2_token_id=57447761'
+                      type='video/mp4'
+                    />
                   </video>
                   <div className='author-img__wrapper video-author'>
                     <svg
@@ -246,14 +264,21 @@ const HomePageContent: React.FC = () => {
                   </div>
                 </div>
                 <div className='video-by offline'>Gerard Bind</div>
-                <div className='video-name'>Prepare for your first skateboard jump</div>
-                <div className='video-view'>42K views<span className='seperate video-seperate'></span>1 week ago</div>
+                <div className='video-name'>
+                  Prepare for your first skateboard jump
+                </div>
+                <div className='video-view'>
+                  42K views<span className='seperate video-seperate'></span>1 week ago
+                </div>
               </div>
               <div className='video anim' style='--delay: .6s'>
                 <div className='video-time'>7 min</div>
                 <div className='video-wrapper'>
                   <video muted=''>
-                    <source src='https://player.vimeo.com/external/436553499.sd.mp4?s=0e44527f269278743db448761e35c5e39cfaa52c&profile_id=139&oauth2_token_id=57447761' type='video/mp4' />
+                    <source
+                      src='https://player.vimeo.com/external/436553499.sd.mp4?s=0e44527f269278743db448761e35c5e39cfaa52c&profile_id=139&oauth2_token_id=57447761'
+                      type='video/mp4'
+                    />
                   </video>
                   <div className='author-img__wrapper video-author'>
                     <svg
@@ -274,14 +299,21 @@ const HomePageContent: React.FC = () => {
                   </div>
                 </div>
                 <div className='video-by offline'>John Wise</div>
-                <div className='video-name'>Basic equipment to play skateboard safely</div>
-                <div className='video-view'>64K views<span className='seperate video-seperate'></span>2 week ago</div>
+                <div className='video-name'>
+                  Basic equipment to play skateboard safely
+                </div>
+                <div className='video-view'>
+                  64K views<span className='seperate video-seperate'></span>2 week ago
+                </div>
               </div>
               <div className='video anim' style='--delay: .7s'>
                 <div className='video-time'>6 min</div>
                 <div className='video-wrapper'>
                   <video muted=''>
-                    <source src='https://player.vimeo.com/external/361861493.sd.mp4?s=19d8275ca755d653042a87ef28b2f0b2eabf57d0&profile_id=139&oauth2_token_id=57447761' type='video/mp4' />
+                    <source
+                      src='https://player.vimeo.com/external/361861493.sd.mp4?s=19d8275ca755d653042a87ef28b2f0b2eabf57d0&profile_id=139&oauth2_token_id=57447761'
+                      type='video/mp4'
+                    />
                   </video>
                   <div className='author-img__wrapper video-author'>
                     <svg
@@ -302,15 +334,33 @@ const HomePageContent: React.FC = () => {
                   </div>
                 </div>
                 <div className='video-by'>Budi Hakim</div>
-                <div className='video-name'>Tips to playing skateboard on the ramp</div>
-                <div className='video-view'>50K views<span className='seperate video-seperate'></span>1 week ago</div>
+                <div className='video-name'>
+                  Tips to playing skateboard on the ramp
+                </div>
+                <div className='video-view'>
+                  50K views<span className='seperate video-seperate'></span>1 week ago
+                </div>
               </div>
             </div>
             <div className='stream-area'>
               <div className='video-stream'>
-                <video id='my_video_1' className='video-js vjs-default-skin anim' width='640px' height='267px' controls preload='none' poster='https://images.unsplash.com/photo-1476801071117-fbc157ae3f01?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDh8fHxlbnwwfHx8&w=1000&q=80'>
-                  <source src='https://player.vimeo.com/external/390402719.sd.mp4?s=20cfdb066c4253047562b65bd4e411b86a004bc5&profile_id=139&oauth2_token_id=57447761' type='video/mp4' />
-                  <source src='https://player.vimeo.com/external/390402719.sd.mp4?s=20cfdb066c4253047562b65bd4e411b86a004bc5&profile_id=139&oauth2_token_id=57447761' type='video/webm' />
+                <video 
+                  id='my_video_1'
+                  className='video-js vjs-default-skin anim'
+                  width='640px'
+                  height='267px'
+                  controls
+                  preload='none'
+                  poster='https://images.unsplash.com/photo-1476801071117-fbc157ae3f01?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDh8fHxlbnwwfHx8&w=1000&q=80'
+                >
+                  <source
+                    src='https://player.vimeo.com/external/390402719.sd.mp4?s=20cfdb066c4253047562b65bd4e411b86a004bc5&profile_id=139&oauth2_token_id=57447761'
+                    type='video/mp4'
+                  />
+                  <source
+                    src='https://player.vimeo.com/external/390402719.sd.mp4?s=20cfdb066c4253047562b65bd4e411b86a004bc5&profile_id=139&oauth2_token_id=57447761'
+                    type='video/webm'
+                  />
                 </video>
                 <div className='video-detail'>
                   <div className='video-content'>
