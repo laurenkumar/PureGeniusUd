132a13c14f73a8f3a2ac785b110e5466ccfbb081
laurenkumar <lauren_d@outlook.fr>
2022-05-18 15:58:57 +0200

    first commit

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -7,7 +7,6 @@ const HomePage: React.FC = () => {
   return (
     <HomePageStyle>
       <PageLayout>
-        <Title>PureGenius</Title>
         <HomePageContent />
       </PageLayout>
     </HomePageStyle>
