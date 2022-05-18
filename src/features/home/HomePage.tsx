43a693741396510a54efbf2599affa16b1e5744b
laurenkumar <lauren_d@outlook.fr>
2022-05-18 15:13:39 +0200

    first commit

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -233,8 +233,8 @@ const HomePageContent: React.FC = () => {
                   Basic how to ride your skateboard comfortly
                 </div>
                 <div className='video-view'>
-                  54K views<span className='seperate video-seperate'></span>
-                  1 week ago
+                  54K views<span className='seperate video-seperate'></span>1
+                   week ago
                 </div>
               </div>
               <div className='video anim' style='--delay: .5s'>
@@ -269,8 +269,8 @@ const HomePageContent: React.FC = () => {
                   Prepare for your first skateboard jump
                 </div>
                 <div className='video-view'>
-                  42K views<span className='seperate video-seperate'></span>
-                  1 week ago
+                  42K views<span className='seperate video-seperate'></span>1
+                   week ago
                 </div>
               </div>
               <div className='video anim' style='--delay: .6s'>
@@ -305,8 +305,8 @@ const HomePageContent: React.FC = () => {
                   Basic equipment to play skateboard safely
                 </div>
                 <div className='video-view'>
-                  64K views<span className='seperate video-seperate'></span>
-                  2 week ago
+                  64K views<span className='seperate video-seperate'></span>2
+                   week ago
                 </div>
               </div>
               <div className='video anim' style='--delay: .7s'>
@@ -341,8 +341,8 @@ const HomePageContent: React.FC = () => {
                   Tips to playing skateboard on the ramp
                 </div>
                 <div className='video-view'>
-                  50K views<span className='seperate video-seperate'></span>
-                  1 week ago
+                  50K views<span className='seperate video-seperate'></span>1
+                   week ago
                 </div>
               </div>
             </div>
@@ -410,7 +410,8 @@ const HomePageContent: React.FC = () => {
                             <path
                               fill-rule='evenodd'
                               clipRule='evenodd'
-                              d='M15.85 2.5c.63 0 1.26.09 1.86.29 3.69 1.2 5.02 5.25 3.91 8.79a12.728 12.728 0 01-3.01 4.81 38.456 38.456 0 01-6.33 4.96l-.25.15-.26-.16a38.093 38.093 0 01-6.37-4.96 12.933 12.933 0 01-3.01-4.8c-1.13-3.54.2-7.59 3.93-8.81.29-.1.59-.17.89-.21h.12c.28-.04.56-.06.84-.06h.11c.63.02 1.24.13 1.83.33h.06c.04.02.07.04.09.06.22.07.43.15.63.26l.38.17c.092.05.195.125.284.19.056.04.107.077.146.1l.05.03c.085.05.175.102.25.16a6.263 6.263 0 013.85-1.3zm2.66 7.2c.41-.01.76-.34.79-.76v-.12a3.3 3.3 0 00-2.11-3.16.8.8 0 00-1.01.5c-.14.42.08.88.5 1.03.64.24 1.07.87 1.07 1.57v.03a.86.86 0 00.19.62c.14.17.35.27.57.29z' />
+                              d='M15.85 2.5c.63 0 1.26.09 1.86.29 3.69 1.2 5.02 5.25 3.91 8.79a12.728 12.728 0 01-3.01 4.81 38.456 38.456 0 01-6.33 4.96l-.25.15-.26-.16a38.093 38.093 0 01-6.37-4.96 12.933 12.933 0 01-3.01-4.8c-1.13-3.54.2-7.59 3.93-8.81.29-.1.59-.17.89-.21h.12c.28-.04.56-.06.84-.06h.11c.63.02 1.24.13 1.83.33h.06c.04.02.07.04.09.06.22.07.43.15.63.26l.38.17c.092.05.195.125.284.19.056.04.107.077.146.1l.05.03c.085.05.175.102.25.16a6.263 6.263 0 013.85-1.3zm2.66 7.2c.41-.01.76-.34.79-.76v-.12a3.3 3.3 0 00-2.11-3.16.8.8 0 00-1.01.5c-.14.42.08.88.5 1.03.64.24 1.07.87 1.07 1.57v.03a.86.86 0 00.19.62c.14.17.35.27.57.29z'
+                            />
                           </svg>
                           Liked
                         </button>
@@ -422,13 +423,14 @@ const HomePageContent: React.FC = () => {
                     <div className='video-p-subtitle anim' style='--delay: .3s'>
                       Lorem ipsum dolor sit amet consectetur adipisicing elit.
                       Repellendus illum tempora consequuntur. Lorem ipsum dolor
-                      sit amet consectetur adipisicing elit. Debitis earum velit accusantium maiores qui sit quas, laborum voluptatibus 
+                      sit amet consectetur adipisicing elit. Debitis earum velit 
+                      accusantium maiores qui sit quas, laborum voluptatibus 
                       vero quidem tempore facilis voluptate tempora deserunt!
                     </div>
                     <div className='video-p-subtitle anim' style='--delay: .4s'>
                       Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Temporibus laborum qui dolorum fugiat eius accusantium 
-                      repellendus illum tempora consequuntur. Lorem ipsum dolor, 
+                      Temporibus laborum qui dolorum fugiat eius accusantium
+                      repellendus illum tempora consequuntur. Lorem ipsum dolor,
                       sit amet consectetur adipisicing elit.
                     </div>
                   </div>
@@ -473,7 +475,9 @@ const HomePageContent: React.FC = () => {
                         />
                       </div>
                       <div className='msg-wrapper'>
-                        <div className='msg__name video-p-name'> Wijaya Adabi</div>
+                        <div className='msg__name video-p-name'>
+                          Wijaya Adabi
+                        </div>
                         <div className='msg__content video-p-sub'>
                           Lorem ipsum clor sit, ame conse quae debitis
                         </div>
@@ -498,8 +502,12 @@ const HomePageContent: React.FC = () => {
                         />
                       </div>
                       <div className='msg-wrapper'>
-                        <div className='msg__name video-p-name offline'> Johny Wise</div>
-                        <div className='msg__content video-p-sub'> Suscipit eos atque voluptates labore</div>
+                        <div className='msg__name video-p-name offline'>
+                          Johny Wise
+                        </div>
+                        <div className='msg__content video-p-sub'>
+                          Suscipit eos atque voluptates labore
+                        </div>
                       </div>
                     </div>
                     <div className='message anim' style='--delay: .3s'>
@@ -521,8 +529,12 @@ const HomePageContent: React.FC = () => {
                         />
                       </div>
                       <div className='msg-wrapper'>
-                        <div className='msg__name video-p-name offline'> Budi Hakim</div>
-                        <div className='msg__content video-p-sub'>Dicta quidem sunt adipisci</div>
+                        <div className='msg__name video-p-name offline'>
+                          Budi Hakim
+                        </div>
+                        <div className='msg__content video-p-sub'>
+                          Dicta quidem sunt adipisci
+                        </div>
                       </div>
                     </div>
                     <div className='message anim' style='--delay: .4s'>
@@ -544,8 +556,12 @@ const HomePageContent: React.FC = () => {
                         />
                       </div>
                       <div className='msg-wrapper'>
-                        <div className='msg__name video-p-name'> Thomas Hope</div>
-                        <div className='msg__content video-p-sub'> recusandae doloremque aperiam alias molestias</div>
+                        <div className='msg__name video-p-name'>
+                          Thomas Hope
+                        </div>
+                        <div className='msg__content video-p-sub'>
+                          recusandae doloremque aperiam alias molestias
+                        </div>
                       </div>
                     </div>
                     <div className='message anim' style='--delay: .5s'>
@@ -567,8 +583,12 @@ const HomePageContent: React.FC = () => {
                         />
                       </div>
                       <div className='msg-wrapper'>
-                        <div className='msg__name video-p-name'> Gerard Will</div>
-                        <div className='msg__content video-p-sub'>Dicta quidem sunt adipisci</div>
+                        <div className='msg__name video-p-name'>
+                          Gerard Will
+                        </div>
+                        <div className='msg__content video-p-sub'>
+                          Dicta quidem sunt adipisci
+                        </div>
                       </div>
                     </div>
                     <div className='message anim' style='--delay: .6s'>
@@ -590,8 +610,12 @@ const HomePageContent: React.FC = () => {
                         />
                       </div>
                       <div className='msg-wrapper'>
-                        <div className='msg__name video-p-name offline'>Johny Wise</div>
-                        <div className='msg__content video-p-sub'> recusandae doloremque aperiam alias molestias</div>
+                        <div className='msg__name video-p-name offline'>
+                          Johny Wise
+                        </div>
+                        <div className='msg__content video-p-sub'>
+                          recusandae doloremque aperiam alias molestias
+                        </div>
                       </div>
                     </div>
                   </div>
